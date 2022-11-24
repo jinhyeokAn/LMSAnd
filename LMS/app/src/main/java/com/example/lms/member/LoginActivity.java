8ce803c3c867145afc3329645ab3bedfe632f16a
wjddmswls11 <wjddmswls10@naver.com>
2022-11-24 09:59:18 +0900

    fg
    ffdhfd

--- LMS/app/src/main/java/com/example/lms/member/LoginActivity.java
+++ LMS/app/src/main/java/com/example/lms/member/LoginActivity.java
@@ -52,6 +52,7 @@ public class LoginActivity extends AppCompatActivity {
                                 Toast.makeText(LoginActivity.this, "vo값이 없습니다", Toast.LENGTH_SHORT).show();
                             }else {
                                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra("vo", vo);
                                 startActivity(intent);
                             }
                         }
