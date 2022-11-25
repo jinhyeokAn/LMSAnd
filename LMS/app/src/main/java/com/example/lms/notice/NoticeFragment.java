afafebc4ef407aa87e37009c4302191c7dc156b3
kimseoine <potato32122@gmail.com>
2022-11-25 14:22:27 +0900

    Revert "아직 받지 마세요"
    This reverts commit 631b600ea6a0fb4516cc2fc7f13e7b7c8c41d7cc.

--- LMS/app/src/main/java/com/example/lms/notice/NoticeFragment.java
+++ LMS/app/src/main/java/com/example/lms/notice/NoticeFragment.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ExpandableListView;
 
 import com.example.lms.R;
 import com.example.lms.lms.CommonAskTask;
@@ -20,12 +19,12 @@ import com.google.gson.reflect.TypeToken;
 import java.util.ArrayList;
 
 public class NoticeFragment extends Fragment {
-    ExpandableListView exp_notice;
+    RecyclerView notice_recv;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_notice, container, false);
-        exp_notice = v.findViewById(R.id.exp_notice);
+        notice_recv = v.findViewById(R.id.notice_recv);
 
 
 
@@ -34,31 +33,40 @@ public class NoticeFragment extends Fragment {
 
 
 
-        notice_select();
+
 
         return v;
     }
 
 
-
+    @Override
+    public void onResume() {
+        super.onResume();
+        notice_select();
+    }
 
     public void notice_select(){
 
-        CommonAskTask askTask = new CommonAskTask("notice.list",getContext());
-        askTask.executeAsk(new CommonAskTask.AsynckTaskCallback() {
+        CommonAskTask task = new CommonAskTask("notice.list" , getContext());
+        task.executeAsk(new CommonAskTask.AsynckTaskCallback() {
             @Override
             public void onResult(String data, boolean isResult) {
-                Log.d("공지사항", "onResult: "+data);
-                if (isResult) {
+                // 통신 완료 시 데이터를 가지고 온다.
+                if(isResult){
                     ArrayList<NoticeVO> list =
-                            new Gson().fromJson(data, new TypeToken<ArrayList<NoticeVO>>() {
-                            }.getType());
-                    Log.d("공지사항", "onResult: " + list.size());
-                    ArrayList<NoticeVO> parentList = new ArrayList<>();
-                    int parentPosition = 0 ;
+                            new Gson().fromJson(data , new TypeToken<ArrayList<NoticeVO>>(){}.getType());
 
-                    NoticeAdapter adapter = new NoticeAdapter(getLayoutInflater(), parentList);
-                    exp_notice.setAdapter(adapter);
+                    NoticeAdapter adapter = new NoticeAdapter(getLayoutInflater() , list , NoticeFragment.this);
+                    RecyclerView.LayoutManager manager = new LinearLayoutManager(
+                            getContext() , RecyclerView.VERTICAL , false
+                    );
+                    notice_recv.setAdapter(adapter);
+                    notice_recv.setLayoutManager(manager);
+
+
+
+                }else {
+                    Log.d("고객", "onResult:Fail " + data);
                 }
             }
         });
