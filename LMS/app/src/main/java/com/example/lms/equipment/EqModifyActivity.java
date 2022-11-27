3671266119c81f7d68e15aa46ff1845ac7df9695
kimseoine <potato32122@gmail.com>
2022-11-27 21:08:33 +0900

    비품 리스트 출력

---
+++ LMS/app/src/main/java/com/example/lms/equipment/EqModifyActivity.java
@@ -0,0 +1,16 @@
+package com.example.lms.equipment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lms.R;
+
+public class EqModifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eq_modify);
+    }
+}
