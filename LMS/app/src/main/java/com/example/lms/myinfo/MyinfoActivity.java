ac0878cf360cd0de705a7a0d0bd3b9f57d31e88c
kimseoine <potato32122@gmail.com>
2022-11-28 21:14:20 +0900

    ㅇ
    ㅇ

---
+++ LMS/app/src/main/java/com/example/lms/myinfo/MyinfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.lms.myinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lms.R;
+
+public class MyinfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myinfo);
+    }
+}
